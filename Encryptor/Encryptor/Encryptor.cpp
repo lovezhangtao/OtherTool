926eeca6ec1d3a618935deed51ba62b2d9d03af1
tony_zhang <tony_zhang@realsil.com.cn>
2023-05-29 15:41:09 +0800

    add xor encrypt
    Signed-off-by: tony_zhang <tony_zhang@realsil.com.cn>

--- Encryptor/Encryptor/Encryptor.cpp
+++ Encryptor/Encryptor/Encryptor.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include "CommandParser.h"
 
+//https://en.wikipedia.org/wiki/XOR_cipher
+//https://cplusplus.com/forum/windows/128374/
+std::string encrypt(std::string msg, std::string key)
+{
+    // Make sure the key is at least as long as the message
+    std::string tmp(key);
+    while (key.size() < msg.size())
+        key += tmp;
 
-int main(int argc, char* argv[]) 
+    // And now for the encryption part
+    for (std::string::size_type i = 0; i < msg.size(); ++i)
+        msg[i] ^= key[i];
+    return msg;
+}
+std::string decrypt(std::string msg, std::string key)
+{
+    return encrypt(msg, key); // lol
+}
+
+int main()
+{
+    std::string message = encrypt("Hello world!", "monkey");
+    std::cout << "Encrypted: " << message;
+    std::cout << "\nDecrypted: " << decrypt(message, "monkey");
+}
+
+int main2(int argc, char* argv[]) 
 {
     CommandParser cmdParser;
     cmdParser.registerCommand("-e", "Encrypt File");
